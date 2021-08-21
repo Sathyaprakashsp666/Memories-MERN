57d06ef316aaa230d44e77639bb65725f7618bef
Sathya Prakasha P <sathyaprakash.sp.6@gmail.com>
2021-08-21 23:20:06 +0530

    env file added

--- server/index.js
+++ server/index.js
@@ -2,26 +2,26 @@ import mongoose from "mongoose";
 import cors from "cors";
 import express from "express";
 import bodyParser from "body-parser";
+import dotenv from 'dotenv'
+
 
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
+dotenv.config()
 
 import postRoutes from "./routes/post.js";
 
 app.use("/posts", postRoutes);
 
 //connecting mongodb cloud
-//mongodb+srv://sathya06:sathya06@cluster0.cunjy.mongodb.net/memories-app?retryWrites=true&w=majority
-//mongodb+srv://ixigo:ixigo@cluster0.dx5wc.mongodb.net/ixigo?retryWrites=true&w=majority
-const MONGO_URL =
-  "mongodb+srv://sathya06:sathya06@cluster0.cunjy.mongodb.net/memories-app?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 mongoose
-  .connect(MONGO_URL, {
+  .connect(CONNECTION_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
